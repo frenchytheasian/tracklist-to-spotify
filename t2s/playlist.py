e3b362cc1e244213059a215e51668f4dab18b350
Michael French <michael.x.french@gmail.com>
2021-08-01 12:43:02 -0500

    Fixed duplication error

--- t2s/playlist.py
+++ t2s/playlist.py
@@ -3,7 +3,7 @@ import sys
 from trackinfo import TrackInfo
 
 import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 # TODO: Maybe create the functionality to create a bunch of playlist from a 
 # list of tracklists. This probably isn't practical though, because no one 
@@ -18,21 +18,31 @@ def create_playlist():
     tracklist_url = sys.argv[1]
 
     ti = TrackInfo(tracklist_url)
+    scope = "playlist-modify-private playlist-modify-public"
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+
+    playlist = sp.user_playlist_create('michael.x.french', ti.get_tracklist_title())
+    songs_to_add = []
     tracks = ti.tracks
-
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-
-    userID = "test"
-    playlist = sp.user_playlist_create(userID, ti.get_tracklist_title())
-    l = []
+    count = 0
 
     for track in tracks:
         search = sp.search(q=track,limit=1)
-        uri = search['tracks']['items'][0]['uri']
-        track = sp.track(uri)
-        l.append(track)
+        track_json = search['tracks']['items']
 
-    sp.user_playlist_add_tracks(userID, playlist.id, l)
+        if track_json:
+            # Sometimes the search is turning up empty
+            uri = track_json[0]['uri']
+            songs_to_add.append(uri)
+
+        if len(songs_to_add) == 100:
+            sp.playlist_add_items(playlist['id'], songs_to_add)
+            songs_to_add = []
+
+        print(f"{count}\{len(tracks)}")
+        count += 1
+    
+    sp.playlist_add_items(playlist['id'], songs_to_add)
 
 if __name__ == '__main__':
     create_playlist()
--- t2s/trackinfo.py
+++ t2s/trackinfo.py
@@ -49,7 +49,8 @@ class TrackInfo:
 
 def main():
     track = TrackInfo("https://www.1001tracklists.com/tracklist/9l2wdv1/two-friends-big-bootie-mix-018-2020-10-26.html")
-    print(track.get_tracks())
+    for song in track.tracks:
+        print(song)
     
 
 
